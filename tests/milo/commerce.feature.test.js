c9408caa382c426eeea0c96309e2c2af8e18c38d
Milica Micic <cod23684@adobe.com>
2024-08-16 14:02:58 +0200

    Update commerce.feature.test.js

--- tests/milo/commerce.feature.test.js
+++ tests/milo/commerce.feature.test.js
@@ -26,7 +26,7 @@ test.afterEach(async ({ browserName }) =>{
 
 
 test.describe('Commerce feature test suite', () => {
-  // test.use({ userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36' });
+  test.use({ userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36' });
 
   // @Commerce-Price-Term - Validate price with term display
   test(`${features[0].name},${features[0].tags}`, async ({ page, baseURL, browserName }) => {
